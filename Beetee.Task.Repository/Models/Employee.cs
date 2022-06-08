2f43e044c6d4e305f7998bfb879fc7a95775e4a7
avagyansamvel <avagyansamvel07@gmail.com>
2022-06-08 16:35:39 +0400

    Add Repository library, and Employee Model

---
+++ Beetee.Task.Repository/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Beetee.Task.Repository.Models
+{
+    public enum Gender
+    {
+        Male, Female
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birth { get; set; }
+        public Gender Gender { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
